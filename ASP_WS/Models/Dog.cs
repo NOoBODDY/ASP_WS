84a476b7b48176aac47e41e181305a1fb378e2dc
NOoBODDY <offmysoap@gmail.com>
2022-02-27 14:27:31 +0300

    Added avatar for dog

--- ASP_WS/Models/Dog.cs
+++ ASP_WS/Models/Dog.cs
@@ -4,9 +4,14 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Breed Breed { get; set; }
         public DateTime Birthday { get; set; }
 
-        public Owner Owner { get; set; }
+        public string AvatarPath { get; set;}
+
+        public int BreedId { get; set; }
+        public Breed? Breed { get; set; }
+        
+        public int OwnerId { get; set; }
+        public Owner? Owner { get; set; }
     }
 }
--- ASP_WS/Models/Owner.cs
+++ ASP_WS/Models/Owner.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        List<Dog> Dogs { get; set; }
+        public List<Dog> Dogs { get; set; }
 
         public Owner()
         {
